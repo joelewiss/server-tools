42702665a40bab8fb039b0810916f7b8a498d602
Joe Lewis <jal126463@gmail.com>
2021-11-24 14:33:13 -0500

    Updated VM script

--- vm.sh
+++ vm.sh
@@ -35,8 +35,10 @@ END
 usage() {
   echo "VM Control Script"
   echo "Controls"
-  echo "S: Power on"
+  echo "P: Power on"
   echo "O: Power off"
+  echo "S: Switch to VM"
+  echo "W: Switch back to Host"
   echo "K: Attach/Detach Keyboard"
   echo "M: Attach/Detach Mouse"
   echo "D: Start sound daemon (scream)"
@@ -47,37 +49,62 @@ virsh_command() {
   eval "$VIRSH_PREFIX $@"
 }
 
-usage
-while true; do
-  read -s -N 1 C
-  C=${C^^} # convert to uppercase
-
-  if [ "$C" = "S" ]; then
+run_cmd() {
+  C=$1
+  if [ "$C" = "P" ]; then
     virsh_command start $DOMAIN
   elif [ "$C" = "O" ]; then
     virsh_command shutdown $DOMAIN
+  elif [ "$C" = "S" ]; then
+    # Switch from host to vm
+    # Attach keyboard and mouse
+    run_cmd "K"
+    run_cmd "M"
+
+    # Turn off screensaver
+    xset s off
+
+    # Turn off VGA output
+    xrandr --output VGA1 --off
+    xrandr --output HDMI3 --primary
+
+  elif [ "$C" = "W" ]; then
+    # Switch from VM to host
+    # Detach keyboard and mouse
+    run_cmd "K"
+    run_cmd "M"
+
+    # Turn off screensaver
+    xset s on
+
+    # Turn on VGA output
+    xrandr --output VGA1 --primary --mode 1920x1080
+    xrandr --output HDMI3 --mode 1920x1080 --left-of VGA1
+
   elif [ "$C" = "K" ]; then
     # Attach/Detach Keyboard
-    echo $KB_XML > tmp
+    KB_FILE=$PWD/tmp_kbd
+    echo $KB_XML > $KB_FILE
     if [ $KB_ATTACH -eq 0 ]; then
-      virsh_command attach-device $DOMAIN $PWD/tmp --live
+      virsh_command attach-device $DOMAIN $KB_FILE --live
       KB_ATTACH=1
     else
-      virsh_command detach-device $DOMAIN $PWD/tmp --live
+      virsh_command detach-device $DOMAIN $KB_FILE --live
       KB_ATTACH=0
     fi
-    rm tmp
+    rm -f $KB_FILE
   elif [ "$C" = "M" ]; then
     # Attach/Detach Mouse
-    echo $M_XML > tmp
+    M_FILE=$PWD/tmp_m
+    echo $M_XML > $M_FILE
     if [ $M_ATTACH -eq 0 ]; then
-      virsh_command attach-device $DOMAIN $PWD/tmp --live
+      virsh_command attach-device $DOMAIN $M_FILE --live
       M_ATTACH=1
     else
-      virsh_command detach-device $DOMAIN $PWD/tmp --live
+      virsh_command detach-device $DOMAIN $M_FILE --live
       M_ATTACH=0
     fi
-    rm tmp
+    rm -f $M_FILE
   elif [ "$C" = "D" ]; then
     # Start sound daemon
     if pgrep -c scream > /dev/null; then
@@ -90,4 +117,12 @@ while true; do
   elif [ "$C" = "F" ]; then
     pkill scream
   fi
+
+}
+
+usage
+while true; do
+  read -s -N 1 C
+  C=${C^^} # convert to uppercase
+  run_cmd $C
 done
